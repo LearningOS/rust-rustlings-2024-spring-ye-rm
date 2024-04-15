8e4460828b6aa9904e4bc6d8ad19df2138dc2fc4
Wenyu Ye <wenyuye@bupt.edu.cn>
2024-04-15 17:57:46 +0800

    smart points and iter4

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -13,7 +13,7 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
-    (1..=num).fold(0,|fact, ret| {fact*ret})
+    (1..=num).fold(1,|fact, ret| {fact*ret})
 }
 
 #[cfg(test)]
